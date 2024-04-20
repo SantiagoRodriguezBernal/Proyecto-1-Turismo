d485498038b878be97197db7936e2483ccf1d446
Juan Camilo Pacheco <jc.pacheco@uniandes.edu.co>
2024-04-20 18:07:37 -0500

    Funcionando

--- Etapa_2/Backend/app.py
+++ Etapa_2/Backend/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
-from joblib import dump,load
+from urllib import request
+from flask import Flask, jsonify, send_file, request
+from flask_cors import CORS
+from joblib import dump, load
+import pandas as pd
+import dataModel
 import joblib
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
-from num2words import num2words
-import re, string, unicodedata
+import num2words
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -13,22 +16,61 @@ from custom import CustomPreprocessor
 from custom import CustomRegression
 
 app = Flask(__name__)
+CORS(app)
 
-
-modelo = joblib.load("ModeloReview2.joblib")
+# Cargar el modelo y el vectorizador
+modelo = load("ModeloReview2.joblib")
 reg = modelo["model"].model
 vectorizer = modelo["model"].vec
-vectors = vectorizer.transform(['Me gusto demasiado volveria a ir'])
-predict = reg.predict(vectors)
-resultado = predict[0]
-print(resultado)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos del formulario enviado y convertirlos en un objeto DataModel
+        data = request.json
+        # Vectorizar los datos de entrada
+        vectors = vectorizer.transform([data['Textos_espanol']])
+        # Realizar la predicción
+        prediction = reg.predict(vectors)[0]
+        # Convertir la predicción a un tipo compatible con JSON
+        prediction = int(prediction)  # o str(prediction)
+        return jsonify({'prediction': prediction})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+@app.route('/uploadexcel', methods=['POST'])
+def upload_excel():
+    # Verificar si se envió un archivo
+    if 'excel_file' not in request.files:
+        return jsonify({'error': 'No se envió ningún archivo'})
+
+    excel_file = request.files['excel_file']
+
+    # Verificar la extensión del archivo
+    if excel_file.filename.endswith(".xlsx"):
+        # Leer el archivo Excel
+        df = pd.read_excel(excel_file)
+        # Vectorizar los datos de texto
+        vectors = vectorizer.transform(df['texto'])
+        # Realizar las predicciones
+        predictions = reg.predict(vectors)
+        # Agregar las predicciones al DataFrame
+        df['prediction'] = predictions
+        # Guardar el DataFrame con las predicciones en un nuevo archivo Excel temporal
+        temp_filename = 'predicted_data.xlsx'
+        df.to_excel(temp_filename, index=False)
+        # Devolver el archivo Excel generado como respuesta
+        return send_file(temp_filename, as_attachment=True)
+    else:
+        return jsonify({'error': 'El archivo debe tener la extensión .xlsx'})
+    
 @app.route('/')
 def hello_world():
     try:
-        return str(resultado)
+        return str("resultado")
     except Exception as e:
         return f"Error: {str(e)}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
--- Etapa_2/PaginaWeb/paginapredictions/src/PredictionForm.js
+++ Etapa_2/PaginaWeb/paginapredictions/src/PredictionForm.js
@@ -2,71 +2,70 @@ import React, { useState } from "react";
 import "./PredictionForm.css";
 import FileUpload from "./FileUpload";
 
-
 function PredictionForm() {
-  const [textosEspanol, setTextosEspanol] = useState("");
-  const [prediction, setPrediction] = useState(null);
+    const [textosEspanol, setTextosEspanol] = useState("");
+    const [prediction, setPrediction] = useState(null);
 
-  const handlePredictClick = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:5000/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ Review: textosEspanol }),
-      });
+    const handlePredictClick = async () => {
+        try {
+            const response = await fetch("http://127.0.0.1:5000/predict", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ Textos_espanol: textosEspanol }),
+            });
 
-      if (response.ok) {
-        const result = await response.json();
-        setPrediction(result);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+            if (response.ok) {
+                const result = await response.json();
+                setPrediction(result.prediction);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
 
-  const handleFileUpload = async (file) => {
-    try {
-      const formData = new FormData();
-      formData.append("csv_file", file);
-      const response = await fetch("http://127.0.0.1:8000/uploadcsv", {
-        method: "POST",
-        body: formData,
-        redirect: 'follow',
-      });
+    const handleFileUpload = async (file) => {
+        try {
+            const formData = new FormData();
+            formData.append("excel_file", file);
+            const response = await fetch("http://127.0.0.1:5000/uploadexcel", {
+                method: "POST",
+                body: formData,
+                redirect: "follow",
+            });
 
-      if (response.ok) {
-        // Maneja la respuesta para descargar el archivo resultante
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "predicted_data.csv";
-        a.click();
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+            if (response.ok) {
+                // Manejar la respuesta para descargar el archivo resultante
+                const blob = await response.blob();
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "predicted_data.xlsx";
+                a.click();
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
 
-  return (
-    <div>
-      <textarea
-        placeholder="Textos en español"
-        value={textosEspanol}
-        onChange={(e) => setTextosEspanol(e.target.value)}
-      />
-      <button onClick={handlePredictClick}>Predecir</button>
-      <FileUpload onFileUpload={handleFileUpload} />
-      {prediction !== null && (
+    return (
         <div>
-          <h3>Resultado de la prediccion (con un 50% de precision):</h3>
-          <p>El texto tiene una calificacion de: {prediction}</p>
+            <textarea
+                placeholder="Textos en español"
+                value={textosEspanol}
+                onChange={(e) => setTextosEspanol(e.target.value)}
+            />
+            <button onClick={handlePredictClick}>Predecir</button>
+            <FileUpload onFileUpload={handleFileUpload} />
+            {prediction !== null && (
+                <div>
+                    <h3>Resultado de la predicción:</h3>
+                    <p>El texto tiene una calificación de: {prediction}</p>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default PredictionForm;
